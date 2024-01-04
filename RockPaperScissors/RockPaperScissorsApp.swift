fe0b33cf175480e9c67dca4378cd2d4469898c54
Mykhailo <dmytrivmykhailo2@gmail.com>
2024-01-04 12:31:10 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Михайло Дмитрів on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
